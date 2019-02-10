ece8e531c8dacf929945ba598ac5545138818277
Super Sonic <stevenscotttucker@gmail.com>
2019-02-10 12:13:30 -0500

    sst

--- bamazon.sql
+++ bamazon.sql
@@ -1,124 +1,30 @@
+DROP DATABASE IF EXISTS Bamazon;
+
+-- creates bamazon database
 CREATE DATABASE Bamazon;
+USE Bamazon;
 
-USE bamazon;
+-- table for products
+CREATE TABLE products (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY (item_id)
+);
 
-CREATE TABLE Products (
-ItemID int NOT NULL,
-ProductName varchar(50) NOT NULL,
-DepartmentName varchar(50) NOT NULL,
-Price DECIMAL(4,2) NOT NULL,
-StockQuantity int NOT NULL);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-12345,
-'Duvet Cover',
-'Home',
-89.99,
-25);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-67891,
-'Mens Zip-up Hoodie',
-'Mens Clothing',
-29.99,
-25);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-23456,
-'Glass Catch-all Dish',
-'Home',
-15.99,
-10);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-78912,
-'Record Player',
-'Electronics',
-110.99,
-8);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-34567,
-'High Rise Skinny Jeans - Black',
-'Womens Clothing',
-59.99,
-30);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-89123,
-'Womens Bruce Springsteen Baseball T-shirt',
-'Womens Clothing',
-25.99,
-8);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-45678,
-'Mens Knit Beanie - Blue',
-'Mens Clothing',
-21.99,
-15);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-91234,
-'Striped Jersey Cotton Sheets - Queen',
-'Home',
-49.99,
-10);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-56789,
-'Spiked Dog Leash',
-'Pets',
-25.99,
-10);
-
-INSERT INTO Products (ItemID, ProductName, DepartmentName, Price, StockQuantity) VALUES (
-11234,
-'Shark Kitty Bed',
-'Pets',
-59.99,
-5);
-
-USE bamazon;
-CREATE TABLE Departments(
-DepartmentId int AUTO_INCREMENT,
-PRIMARY KEY(DepartmentId),
-DepartmentName varchar(50) NOT NULL,
-OverHeadCosts DECIMAL(11,2) NOT NULL,
-TotalSales DECIMAL(11,2) NOT NULL);
-
-
-INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Mens Clothing',
-10000,
-0);
-
-INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Pets',
-10000,
-0);
-
-INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Home',
-20000,
-0);
-
-INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Shoes',
-15000,
-0);
-
-INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Electronics',
-50000,
-0);
-
-INSERT INTO Departments (DepartmentName, OverHeadCosts, TotalSales) VALUES (
-'Womens Clothing',
-25000,
-0);
-
-
--- This creates the alias table TotalProfits that will exist only when requested by the executive 
-SHOW TABLES;
-CREATE VIEW bamazon.TotalProfits AS SELECT DepartmentId, DepartmentName, OverHeadCosts, TotalSales, TotalSales-OverHeadCosts AS TotalProfit FROM Departments;
+-- faux product data
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES  ('Sony HDTV', 'Electronics', 699.99, 500),
+		('Martha Stewart Frying Pan', 'Kitchen', 29.99, 222),
+		('Nike T-Shirt', 'Clothing', 15.99, 1000),
+		('Bamazon Basics Copy Paper', 'Office Supplies', 19.99, 500),
+		('Apple Watch', 'Electronics', 499.99, 800),
+		('Tempurpedic Queen Bed', 'Home', 1299, 200),
+		('Whole Earth Cat Food', 'Pet', 1.25, 300),
+		('Bamazon Basics Tripod', 'Electronics', 15.99, 250),
+		('Blue Ocean Above Ground Pool', 'Outdoor', 2500, 100),
+		('BMW 335i', 'Auto', 51995.99, 5),
+		('Space X Falcon Rocket', 'Space', 80000000.00, 100),
+		('Citation Jet', 'Aircraft', 50000000.00, 10);
--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,115 +1,133 @@
+// inquirer and mysql npm
+var inquirer = require('inquirer');
 var mysql = require('mysql');
-var prompt = require('prompt');
-var colors = require('colors/safe');
-var Table = require('cli-table');
+
+// connect to mysql
 var connection = mysql.createConnection({
 	host: 'localhost',
+	port: 3306,
+
 	user: 'root',
+
 	password: '',
-	database: 'Bamazon', 
+	database: 'Bamazon'
 });
 
-var productPurchased = [];
+// positive input validtion
+function validateInput(value) {
+	var integer = Number.isInteger(parseFloat(value));
+	var sign = Math.sign(value);
 
-connection.connect();
-
-//connect to the mysql database and pull the information from the Products database to display to the user
-connection.query('SELECT ItemID, ProductName, Price FROM Products', function(err, result){
-	if(err) console.log(err);
-
-	//creates a table for the information from the mysql database to be placed
-	var table = new Table({
-		head: ['Item Id#', 'Product Name', 'Price'],
-		style: {
-			head: ['blue'],
-			compact: false,
-			colAligns: ['center'],
-		}
-	});
-
-	//loops through each item in the mysql database and pushes that information into a new row in the table
-	for(var i = 0; i < result.length; i++){
-		table.push(
-			[result[i].ItemID, result[i].ProductName, result[i].Price]
-		);
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+		return 'Please enter a whole non-zero number.';
 	}
-	console.log(table.toString());
+}
 
-	purchase();
-});
+// prompt for purchase
+function promptUserPurchase() {
 
-//the purchase function so the user can purchase one of the items listed above
-var purchase = function(){
-
-	//creates the questions that will be prompted to the user
-	var productInfo = {
-		properties: {
-			itemID:{description: colors.blue('Please enter the ID # of the item you wish to purchase!')},
-			Quantity:{description: colors.green('How many items would you like to purchase?')}
+	// user select an item
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'item_id',
+			message: 'Please enter the Item ID you would like to purchase.',
+			validate: validateInput,
+			filter: Number
 		},
-	};
+		{
+			type: 'input',
+			name: 'quantity',
+			message: 'How many do you need?',
+			validate: validateInput,
+			filter: Number
+		}
+	]).then(function(input) {
 
-	prompt.start();
+		var item = input.item_id;
+		var quantity = input.quantity;
 
-	//gets the responses to the prompts above
-	prompt.get(productInfo, function(err, res){
+		// confirm item is in db
+		var queryStr = 'SELECT * FROM products WHERE ?';
 
-		//places these responses in the variable custPurchase
-		var custPurchase = {
-			itemID: res.itemID,
-			Quantity: res.Quantity
-		};
-		
-		//the variable established above is pushed to the productPurchased array defined at the top of the page
-		productPurchased.push(custPurchase);
+		connection.query(queryStr, {item_id: item}, function(err, data) {
+			if (err) throw err;
 
-		//connects to the mysql database and selects the item the user selected above based on the item id number entered
-		connection.query('SELECT * FROM Products WHERE ItemID=?', productPurchased[0].itemID, function(err, res){
-				if(err) console.log(err, 'That item ID doesn\'t exist');
-				
-				//if the stock quantity available is less than the amount that the user wanted to purchase then the user will be alerted that the product is out of stock
-				if(res[0].StockQuantity < productPurchased[0].Quantity){
-					console.log('That product is out of stock!');
-					connection.end();
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+				displayInventory();
 
-				//otherwise if the stock amount available is more than or equal to the amount being asked for then the purchase is continued and the user is alerted of what items are being purchased, how much one item is and what the total amount is
-				} else if(res[0].StockQuantity >= productPurchased[0].Quantity){
+			} else {
+				var productData = data[0];
 
-					console.log('');
+				// item in stock
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product you requested is in stock! Placing order!');
 
-					console.log(productPurchased[0].Quantity + ' items purchased');
+					// query string
+					var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
 
-					console.log(res[0].ProductName + ' ' + res[0].Price);
+					// inventory update
+					connection.query(updateQueryStr, function(err, data) {
+						if (err) throw err;
 
-					//this creates the variable SaleTotal that contains the total amount the user is paying for this total puchase
-					var saleTotal = res[0].Price * productPurchased[0].Quantity;
-
-					//connect to the mysql database Departments and updates the saleTotal for the id of the item purchased
-					connection.query("UPDATE Departments SET TotalSales = ? WHERE DepartmentName = ?;", [saleTotal, res[0].DepartmentName], function(err, resultOne){
-						if(err) console.log('error: ' + err);
-						return resultOne;
-					})
-
-					console.log('Total: ' + saleTotal);
-
-					//this variable contains the newly updated stock quantity of the item purchased
-					newQuantity = res[0].StockQuantity - productPurchased[0].Quantity;
-			
-					// connects to the mysql database products and updates the stock quantity for the item puchased
-					connection.query("UPDATE Products SET StockQuantity = " + newQuantity +" WHERE ItemID = " + productPurchased[0].itemID, function(err, res){
-						// if(err) throw err;
-						// console.log('Problem ', err);
-						console.log('');
-						console.log(colors.cyan('Your order has been processed.  Thank you for shopping with us!'));
-						console.log('');
+						console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for shopping with Bamazon!');
+						console.log("\n---------------------------------------------------------------------\n");
 
 						connection.end();
 					})
+				} else {
+					console.log('Unfortunately, we are out of stock.');
+					console.log('Please modify your order.');
+					console.log("\n---------------------------------------------------------------------\n");
 
-				};
-
+					displayInventory();
+				}
+			}
 		})
 	})
+}
 
-};
+// correct inventory 
+function displayInventory() {
 
+	// db query string
+	queryStr = 'SELECT * FROM products';
+
+	// db query
+	connection.query(queryStr, function(err, data) {
+		if (err) throw err;
+
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+
+		var strOut = '';
+		for (var i = 0; i < data.length; i++) {
+			strOut = '';
+			strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+			strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+			strOut += 'Department: ' + data[i].department_name + '  //  ';
+			strOut += 'Price: $' + data[i].price + '\n';
+
+			console.log(strOut);
+		}
+
+	  	console.log("---------------------------------------------------------------------\n");
+
+	  	// item purchase prompt
+	  	promptUserPurchase();
+	})
+}
+
+// business logic
+function runBamazon() {
+
+	// inventory available
+	displayInventory();
+}
+
+// bamazon function
+runBamazon();
